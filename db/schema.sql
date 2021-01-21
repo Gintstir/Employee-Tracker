1621d4778002a7c7d41a9bccbdbc2a2b52f9659b
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-01-21 11:22:04 -0500

    created database employee_db, used source command to create tables and insert data

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,16 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
+    salary DECIMAL(10),
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
-INSERT INTO department (name)
+USE employee_db;
+
+INSERT INTO department (department_name)
 VALUES
     ("Sales"),
     ("Engineering"),
@@ -14,13 +16,23 @@ VALUES
     ("Head Engineer", 72000, 2),
     ("Accountant", 80000, 3),
     ("Director of Human Resources", 112000, 4),
-    ("Human Resources Coordinator", 60000, 5),
-    ("Business Development Manager", 80000, 6),
-    ("Customer Care Specialist", 55000, 7),
-    ("Lawyer", 96000, 8);
+    ("Human Resources Coordinator", 60000, 4),
+    ("Business Development Manager", 80000, 5),
+    ("Customer Care Specialist", 55000, 6),
+    ("Lawyer", 96000, 7);
 
-INSERT INTO employee (first_name, last_name, manager_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES 
-    ("Sara", "Hitchens")
+    ("James", "Joyce", 1),
+    ("Gabriel", "Marquez", 3),
+    ("Dianna", "Burgess", 2),
+    ("Faith", "Rodriguez", 6),
+    ("Earl", "Harris", 6),
+    ("Davin", "Anderson", 7),
+    ("Shelly", "Bowen", 4),
+    ("Elmer", "Munoz", 5),
+    ("Marion", "Jimenez", 1),
+    ("Wesley", "Webster", 1),
+    ("Alice", "George", 7);
 
 
